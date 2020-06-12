dab556433b68e2c572c3121a397b52c41165c357
bdavis90 <wowzerss90@yahoo.com>
2020-06-11 21:47:57 -0400

    finished single_number and moving_zeroes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,10 +4,25 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    num_dict = {}
 
-    pass
+    for num in arr:
+        if num in num_dict:
+            num_dict[num] += 1
+        else:
+            num_dict[num] = 1
 
+    for single in num_dict:
+        if num_dict[single] == 1:
+            return single
+            
+    
+    
+    
+        
 
+arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+print(single_number(arr))
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,9 +4,20 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    max_sum = 0
+    for i in range(k):
+        # print(i)
+        max_sum += nums[i]
+        print(max_sum, 'max sum')
+    
+        # for start in range(1, i - k):
+        #     print(start)
 
-    pass
 
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+print(sliding_window_max(arr, k))
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
